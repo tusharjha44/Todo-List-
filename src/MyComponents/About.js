0a7e84f17e6e1b6b9afa3c7e6582938dbef35d00
tusharjha44 <85125680+tusharjha44@users.noreply.github.com>
2023-10-20 03:37:45 +0530

    First react app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is My about page
+      <p>loeposkrjiouvhbo ckneauihcb9hwbb dkjabcvuvhsaocjn liofsrhnuivb jnbviyhoi nkb iorsnv i
+        dlknviubn mvuibruhdnocjsiovn ilk niohniohn kj bidsbui jns dkjib iun lkb ui kjslb k iokd 
+        d kjsbi bjskdnvoinsdlknvion ksldnvoins ksldnvion  lsknoin lkslvnoisn; lksnoin nflkinvi
+      </p>
+    </div>
+  )
+}
+
+export default About
